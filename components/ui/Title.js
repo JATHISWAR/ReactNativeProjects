c042aa5cad241a0fe78b7c853bc1c4a1bfb6dfc9
jathiswarbhaskar <codguy2802@gmail.com>
2023-01-04 12:28:58 +0530

    final game

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,6 +1,4 @@
-import { Text, StyleSheet } from "react-native";
-
-import Colors from "../../constants/colors";
+import { Text, StyleSheet } from 'react-native';
 
 function Title({ children }) {
   return <Text style={styles.title}>{children}</Text>;
@@ -10,9 +8,10 @@ export default Title;
 
 const styles = StyleSheet.create({
   title: {
+    fontFamily: 'open-sans-bold',
     fontSize: 24,
-    fontWeight: 'bold',
-    color: ' white',
+    // fontWeight: 'bold',
+    color: 'white',
     textAlign: 'center',
     borderWidth: 2,
     borderColor: 'white',
